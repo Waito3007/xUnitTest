4831e7435650047bea328cd5bac11ea181cd8f22
San <sangvu2015dp1@gmai.com>
2025-10-28 16:10:12 +0700

    Refactor UserServiceTest to use IUserRepository mock and improve test methods for GetUserNameAsync and GetById

--- LearnUnitTest/UnitTestPractice.Tests/UserServiceTest.cs
+++ LearnUnitTest/UnitTestPractice.Tests/UserServiceTest.cs
@@ -1,23 +1,45 @@
 ﻿using LearnUnitTest.UnitTestPractice.Interface;
+using LearnUnitTest.UnitTestPractice.Model;
 using Moq;
 
 namespace LearnUnitTest.UnitTestPractice.Tests;
 
 public class UserServiceTest
 {
-    private readonly Mock<IUserService> _userServiceMock;
+    private readonly Mock<IUserRepository> _repoMock;
+    private readonly UserService _userService; 
+
     public UserServiceTest()
     {
-        _userServiceMock = new Mock<IUserService>();
-
+        _repoMock = new Mock<IUserRepository>();
+        _userService = new UserService(_repoMock.Object);
     }
 
     [Fact]
-    public void UserServiceTesting()
+    public async Task GetUserNameAsync_ReturnsName()
     {
-        _userServiceMock.Setup(x => x.GetUserNameAsync(1)).ReturnsAsync("1,Sang");
-        var userService = _userServiceMock.Object;
-        var result = userService.GetUserNameAsync(1).Result;
-        Assert.Equal("1,Sang", result);
+        // Arrange 
+        _repoMock.Setup(x => x.GetById(1))
+            .ReturnsAsync(new User { Id = 1, Name = "Sang" });
+
+        // Act
+        var result = await _userService.GetUserNameAsync(1);
+
+        // Assert
+        Assert.Equal("Sang", result);
+        _repoMock.Verify(r => r.GetById(1), Times.Once);
+    }
+    [Fact]
+    public async Task GetById_User()
+    {
+        // Arrange
+        var user = new User { Id = 1, Name = "Sang" };
+        _repoMock.Setup(x => x.GetById(1))
+            .ReturnsAsync(user);
+        // act
+        var result = await _userService.GetById(1);
+        // assert
+        Assert.Equal(user,result);
+        _repoMock.Verify(r => r.GetById(1), Times.Once);
     }
 }
