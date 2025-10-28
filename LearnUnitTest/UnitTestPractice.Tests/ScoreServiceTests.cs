ae220ddc7df82ba5bfb308c5729983d36e7f924f
San <sangvu2015dp1@gmai.com>
2025-10-28 13:52:25 +0700

    Add ScoreService and corresponding tests for average score calculation

---
+++ LearnUnitTest/UnitTestPractice.Tests/ScoreServiceTests.cs
@@ -0,0 +1,24 @@
+﻿namespace LearnUnitTest.UnitTestPractice.Tests;
+
+public class ScoreServiceTests
+{
+    private readonly ScoreService _scoreService;
+
+    public ScoreServiceTests()
+    {
+        _scoreService = new ScoreService();
+    }
+
+    [Theory]
+    [InlineData(new int[] { 90, 80, 70 }, 80)]
+    [InlineData(new int[] { 100, 100, 100 }, 100)]
+    [InlineData(new int[] { 9, 7, 8 }, 8)]
+    public void AvgScore(int[] scores, int exprectedAvg)
+    {
+        //Arrange
+        //Act
+        var avg = _scoreService.avgScore(scores);
+        // assert
+        Assert.Equal(avg,exprectedAvg);
+    }
+}
---
+++ LearnUnitTest/UnitTestPractice/ScoreService.cs
@@ -0,0 +1,20 @@
+﻿namespace LearnUnitTest.UnitTestPractice;
+
+public class ScoreService
+{
+    public int avgScore(int[] scores)
+    {
+        int total = 0; 
+        for (int i = 0; i < scores.Length; i++)
+        {
+            int score = scores[i];
+            if (score < 0 || score > 10000)
+            {
+                throw new ArgumentException("Score out of range");
+            } 
+            
+            total += score ;
+        }
+        return total / scores.Length;
+    }
+}
